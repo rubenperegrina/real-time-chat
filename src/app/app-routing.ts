b36ae5ab7e2175220f0b44bcd8d35fa29f82209d
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-03-13 16:04:06 +0100

    Add guards to the routes

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -1,22 +1,31 @@
 import { Routes } from '@angular/router';
+import { canActivate, redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 
 export const routes: Routes = [
   {
     path: 'signin',
     title: 'Sign in',
     loadComponent: () =>
-      import('../app/features/auth/sign-in/sign-in.component').then((c) => c.SignInComponent)
+      import('../app/features/auth/sign-in/sign-in.component').then((c) => c.SignInComponent),
+    ...canActivate(() => redirectLoggedInTo(['chat']))
   },
   {
     path: 'signup',
     title: 'Sign up',
     loadComponent: () =>
-      import('../app/features/auth/sign-up/sign-up.component').then((c) => c.SignUpComponent)
+      import('../app/features/auth/sign-up/sign-up.component').then((c) => c.SignUpComponent),
+      ...canActivate(() => redirectLoggedInTo(['chat']))
   },
   {
     path: 'chat',
     title: 'Chat',
     loadComponent: () =>
-      import('../app/features/chat/chat-page/chat-page.component').then((c) => c.ChatPageComponent)
+      import('../app/features/chat/chat-page/chat-page.component').then((c) => c.ChatPageComponent),
+      ...canActivate(() => redirectUnauthorizedTo(['signin']))
   },
+  {
+    path: '',
+    redirectTo: 'chat',
+    pathMatch: 'full'
+  }
 ];
